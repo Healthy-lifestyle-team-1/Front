94479ddb9c88ffb4bbac798869d9b21fd0e1e7d9
Veronika <xafling@mail.ru>
2024-05-29 10:24:13 +0300

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
 import Toggle from './components/ui/Toggle';
+import { HomePage } from './pages/HomePage';
+
+import './App.css';
 
 function App() {
   return (
     <div>
       <Toggle />
       <div>Здесь будет наш прекрасный проект!</div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/userpage" element={<UserPage />} /> */}
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx';
 import ThemeProvider from './core/theme/ThemeProvider';
+
 import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
