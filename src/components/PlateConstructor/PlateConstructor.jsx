17560e2191050d4213766ffe92b8260bd992fbfe
Name <kattie@outlook.com>
2024-06-02 15:46:59 +0200

    new components added, btns props changed

--- src/components/PlateConstructor/PlateConstructor.jsx
+++ src/components/PlateConstructor/PlateConstructor.jsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React from "react";
 
-import cn from 'classnames';
-import s from './styles.module.scss';
+import cn from "classnames";
+import s from "./styles.module.scss";
+import { Button } from "../ui/Button/Button";
 
 export const PlateConstructor = () => {
-
+  const tags = ["глютен", "сахар", "мучное", "лук", "морковь", "ещё"];
 
   return (
     <div className={s.container}>
-      <div className={s.PlateConstructor__title}>Конструктор</div>
+      <div className={s.plateConstructor}>
+        <div className={s.plateConstructor__header}>
+          <div className={s.plateConstructor__title}>Конструктор</div>
+          <div className={s.plateConstructor__description}>
+            Собери свою персональную тарелку
+          </div>
+        </div>
+        <div className={s.plateConstructor__tagBtns}>
+          {/* кнопки-теги */}
+          {tags.map((tag, index) => (
+            <Button key={index} variant="gray" isTag title={tag} />
+          ))}
+        </div>
+        <div className={s.plateConstructor__constructorBlock}>
+          <div
+            className={cn(
+              s.plateConstructor__plateHalf,
+              s["plateConstructor__plateHalf--first"]
+            )}
+          >
+            {/* Первый блок с половинкой тарелки */}
+            <div className={s.plateConstructor__constructorBlock}></div>
+          </div>
+          <div
+            className={cn(
+              s.plateConstructor__plateHalf,
+              s["plateConstructor__plateHalf--second"]
+            )}
+          >
+            {/* Второй блок с половинкой тарелки */}
+          </div>
+        </div>
+      </div>
+      <div className={s.plateConstructor__btn}>
+        <Button
+          className={s.button}
+          width="280px"
+          title="Собрать тарелку"
+          onClick={() => console.log("Button clicked")}
+        >
+          Собрать тарелку
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
---
+++ src/components/ui/Sliders/SliderPlates/SliderPlates.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import img1 from '../../../../assets/images/plates/plates1.jpg';
+
+import cn from 'classnames';
+import s from './styles.module.scss';
+
+export const SliderPlates = ({img, title, description, price}) => {
+
+}
