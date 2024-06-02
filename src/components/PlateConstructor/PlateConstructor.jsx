e5ed2dc50e016e79b37b721ff149ee46c5e7e8a5
Name <kattie@outlook.com>
2024-06-02 11:36:08 +0200

    fonts and color variables added

---
+++ src/components/PlateConstructor/PlateConstructor.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import cn from 'classnames';
+import s from './styles.module.scss';
+
+export const PlateConstructor = () => {
+
+
+  return (
+    <div className={s.container}>
+      <div className={s.PlateConstructor__title}>Конструктор</div>
+    </div>
+  )
+}
