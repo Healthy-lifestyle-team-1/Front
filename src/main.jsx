3773b95593b601d4ef20446435c6a1f08e9f7162
Veronika <xafling@mail.ru>
2024-06-03 00:09:19 +0300

    fixed config

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import App from './App.jsx';
 import {store} from './core/store/store.js';
-// import ThemeProvider from './core/theme/ThemeProvider';
-
 import './index.scss';
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import postcssImport from 'postcss-import';
-import postcssNested from 'postcss-nested';
-import autoprefixer from 'autoprefixer';
+// import postcssImport from 'postcss-import';
+// import postcssNested from 'postcss-nested';
+// import autoprefixer from 'autoprefixer';
+// import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src/styles/variables";`
-      }
-    },
-    postcss: {
-      plugins: [
-        postcssImport,
-        postcssNested,
-        autoprefixer
-      ]
-    }
-  }
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import "@/assets/styles/variables.scss";`
+  //     }
+  //   },
+  //   postcss: {
+  //     plugins: [
+  //       postcssImport,
+  //       postcssNested,
+  //       autoprefixer
+  //     ]
+  //   }
+  // },
+  // resolve: {
+  //   alias: {
+  //     // eslint-disable-next-line no-undef
+  //     '@': path.resolve(__dirname, 'src')
+  //   }
+  // }
 });
