aa55477521acea4b727489a2a07cea8d6129c4c4
Veronika <xafling@mail.ru>
2024-06-03 09:34:35 +0300

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import postcssImport from 'postcss-import';
-// import postcssNested from 'postcss-nested';
-// import autoprefixer from 'autoprefixer';
-// import path from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       additionalData: `@import "@/assets/styles/variables.scss";`
-  //     }
-  //   },
-  //   postcss: {
-  //     plugins: [
-  //       postcssImport,
-  //       postcssNested,
-  //       autoprefixer
-  //     ]
-  //   }
-  // },
-  // resolve: {
-  //   alias: {
-  //     // eslint-disable-next-line no-undef
-  //     '@': path.resolve(__dirname, 'src')
-  //   }
-  // }
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "./src/assets/styles/variables.scss";
+        @import "./src/assets/styles/fonts.scss";
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
