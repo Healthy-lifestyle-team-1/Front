fa4682315d0e177a2174fe1d1d6daa0ddf3692ee
Veronika <xafling@mail.ru>
2024-06-04 15:18:01 +0300

    tune project

--- src/components/ui/Sliders/SliderPlates/SliderPlates.jsx
+++ src/components/ui/Sliders/SliderPlates/SliderPlates.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
-import styles from './styles.module.scss';
+import s from './styles.module.scss';
 import rightImage from '../../../../assets/images/halfofplates/right/Правая.png';
 import leftImage from '../../../../assets/images/halfofplates/left/левая.png';
 
 const NextArrow = props => {
   const { onClick } = props;
   return (
-    <button className={styles.rightArrow} onClick={onClick}>
+    <button className={s.rightArrow} onClick={onClick}>
       ⬆️
     </button>
   );
@@ -18,7 +18,7 @@ const NextArrow = props => {
 const PrevArrow = props => {
   const { onClick } = props;
   return (
-    <button className={styles.leftArrow} onClick={onClick}>
+    <button className={s.leftArrow} onClick={onClick}>
       ⬇️
     </button>
   );
@@ -53,17 +53,17 @@ export const SliderPlates = () => {
   const rightImages = [rightImage, rightImage, rightImage, rightImage];
 
   return (
-    <div className={styles.carouselContainer}>
-      <Slider {...leftSettings} className={styles.leftSlider}>
+    <div className={s.carouselContainer}>
+      <Slider {...leftSettings} className={s.leftSlider}>
         {leftImages.map((img, idx) => (
-          <div key={idx} className={styles.imageContainer}>
+          <div key={idx} className={s.imageContainer}>
             <img src={img} alt={`left ${idx}`} />
           </div>
         ))}
       </Slider>
-      <Slider {...rightSettings} className={styles.rightSlider}>
+      <Slider {...rightSettings} className={s.rightSlider}>
         {rightImages.map((img, idx) => (
-          <div key={idx} className={styles.imageContainer}>
+          <div key={idx} className={s.imageContainer}>
             <img src={img} alt={`right ${idx}`} />
           </div>
         ))}
