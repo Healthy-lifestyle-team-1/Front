4b27fa16e5212aa78e38c39f02a3c91f7828d23f
Name <kattie@outlook.com>
2024-06-04 22:23:16 +0200

    component added

--- src/pages/InProgress/InProgress.jsx
+++ src/pages/InProgress/InProgress.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import cn from 'classnames';
 import s from './styles.module.scss';
+import { InProgressLoader } from '../../components/ui/InProgressLoader';
 
-export function InProgress () {
+export function InProgress() {
   return (
     <div className={s.InProgressContainer}>
       <div className={s.InProgresText}>
         Данная страница находится в процессе разработки
       </div>
+		<InProgressLoader />
     </div>
   );
 }
