3757c708a3ae4c9a2a0614a4381c5cf59aa04e4d
victoria <itsfoxiefo@gmail.com>
2024-06-11 15:13:49 +0200

    tuned CardFood (TODO: ingredients list)

--- src/components/ui/Cards/CardFood/CardFood.jsx
+++ src/components/ui/Cards/CardFood/CardFood.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Button } from '../../../../components/ui/Button/Button';
-import plateImg from '../../../../assets/images/plates/card-food-img.png';
-import line from '../../../../assets/images/dotted-line-card-food.svg';
 import cn from 'classnames';
 import s from './styles.module.scss';
+import DropDown from '../../../../components/ui/DropDown/DropDown';
+import { Checkbox } from '../../Checkbox/Checkbox';
+
+import line from '../../../../assets/images/dotted-line-card-food.svg';
+import plateImg from '../../../../assets/images/plates/card-food-img.png';
 import emptyLike from '../../../../assets/images/icons/light/пустойЛайк.svg';
 import filledLike from '../../../../assets/images/icons/light/закрашенныйЛайк.svg';
-import DropDown from '../../../../components/ui/DropDown/DropDown';
+import chartLight from '../../../../assets/images/chart-light.svg';
+import chartDark from '../../../../assets/images/chart-dark.svg';
 
-// иконки для маркировки продуктов питания
 import vegan from '../../../../assets/images/icons/light/вег.svg';
 import glutenFree from '../../../../assets/images/icons/light/глютенX.svg';
 import lactoseFree from '../../../../assets/images/icons/light/лактозaX.svg';
@@ -24,6 +27,7 @@ export const CardFood = ({
 }) => {
   const descriptions = ['кБЖУ', 'Состав', 'Описание'];
   const [visibleDescriptions, setVisibleDescriptions] = useState([0, 1]);
+  const [activeDescription, setActiveDescription] = useState(2);
   const activeTags = useSelector(state => state.tags);
 
   const handleDescriptionClick = index => {
@@ -34,11 +38,61 @@ export const CardFood = ({
     );
     newVisibleDescriptions[clickedIndex] = remainingDescription;
     setVisibleDescriptions(newVisibleDescriptions);
+    setActiveDescription(index);
   };
 
   const [liked, setLiked] = useState(false);
   const handleLikeClick = () => {
     setLiked(!liked);
+  };
+
+  const ingredients = ['Говядина', 'Картофель', 'Томаты', 'Салат', 'Лук'];
+
+  const [checkedItems, setCheckedItems] = useState(
+    new Array(ingredients.length).fill(true),
+  );
+
+  const handleCheckboxChange = index => {
+    const newCheckedItems = [...checkedItems];
+    newCheckedItems[index] = !newCheckedItems[index];
+    setCheckedItems(newCheckedItems);
+  };
+
+  const renderContent = () => {
+    switch (activeDescription) {
+      case 0:
+        return (
+          <div>
+            <div className={s.cardfood__detailsTitle}>кБЖУ</div>
+            <img src={chartLight} alt="кБЖУ" />;
+          </div>
+        );
+      case 1:
+        return (
+          <div>
+            <span className={s.cardfood__detailsTitle}>Состав</span>
+            <ul>
+              {ingredients.map((ingredient, index) => (
+                <li key={index}>
+                  <Checkbox
+                    checked={checkedItems[index]}
+                    onChange={() => handleCheckboxChange(index)}
+                  />
+                  {ingredient}
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      case 2:
+      default:
+        return (
+          <div>
+            <div className={s.cardfood__detailsTitle}>Описание</div>
+            <div className={s.cardfood__detailsDescription}>{description}</div>
+          </div>
+        );
+    }
   };
 
   return (
@@ -56,10 +110,7 @@ export const CardFood = ({
           <span className={s.cardfood__calories}>{calories}</span>
         </div>
         <div className={s.cardfood__details}>
-          <div className={s.cardfood__detailsText}>
-            <div className={s.cardfood__detailsTitle}>Описание</div>
-            <div className={s.cardfood__detailsDescription}>{description}</div>
-          </div>
+          <div className={s.cardfood__detailsSelect}>{renderContent()}</div>
           <div className={s.cardfood__detailsLine}>
             <img src={line} alt="линия"></img>
           </div>
@@ -79,7 +130,7 @@ export const CardFood = ({
           </div>
         </div>
         <div className={s.cardfood__options}>
-          <DropDown colorScheme={2} options={options} buttonText={'Отзывы'}/>
+          <DropDown colorScheme={2} options={options} buttonText={'Отзывы'} />
           <Button colorScheme={2} title={'В корзину'} size={1} />
         </div>
       </div>
@@ -109,15 +160,6 @@ const plates = [
       'Салат из свежих овощей, с добавлением микро-зелени, приправлен ореховым соусом отличный гарнир на ужин',
     img: plateImg,
   },
-  // {
-  //   title: 'Томленая говядина',
-  //   extra: 'в томатном соусе с молодым картофелем в укропе',
-  //   weight: '560 г',
-  //   calories: '675 ккал',
-  //   description:
-  //     'Салат из свежих овощей, с добавлением микро-зелени, приправлен ореховым соусом отличный гарнир на ужин',
-  //   img: plateImg,
-  // },
 ];
 
 export const CardFoodList = () => {
