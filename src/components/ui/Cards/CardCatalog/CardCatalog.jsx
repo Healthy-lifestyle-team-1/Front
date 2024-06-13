a737fb7ea59178a28e194f5deac1eaa6692ddfc2
victoria <itsfoxiefo@gmail.com>
2024-06-13 02:20:08 +0200

    tuned CardCatalog: it is now ready to be used!

--- src/components/ui/Cards/CardCatalog/CardCatalog.jsx
+++ src/components/ui/Cards/CardCatalog/CardCatalog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { ButtonWithTheme } from '../../Button';
 import cn from 'classnames';
@@ -8,15 +8,20 @@ import line from '../../../../assets/images/dotted-line-card-catalog.svg';
 import plateImg from '../../../../assets/images/halfofplates/left/card-catalog-img.png';
 
 import vegan from '../../../../assets/images/icons/light/вег.svg';
+import veganDark from '../../../../assets/images/icons/dark/вег.svg';
+
 import glutenFree from '../../../../assets/images/icons/light/глютенX.svg';
+import glutenFreeDark from '../../../../assets/images/icons/dark/глютенX.svg';
 
 export const CardCatalog = ({ title, extra, weight, calories, img }) => {
+  const theme = useSelector((state) => state.theme);
+
   return (
     <div className={s.container}>
       <div className={s.cardfood__info}>
         <div className={s.cardfood__labels}>
-          <img src={vegan} alt=""></img>
-          <img src={glutenFree} alt=""></img>
+          <img src={theme === 'dark' ? veganDark : vegan} alt="Растительные продукты"></img>
+          <img src={theme === 'dark' ? glutenFreeDark : glutenFree} alt="Без глютена"></img>
         </div>
         <div className={s.cardfood__title}>{title}</div>
         <div className={s.cardfood__extra}>{extra}</div>
