ed6c7c60ac349045eb78fecd5ee54cdf768730a2
victoria <itsfoxiefo@gmail.com>
2024-06-15 13:54:00 +0200

    tuned NavCatalog

--- src/components/NavCatalog/CatalogLinkButton.jsx
+++ src/components/NavCatalog/CatalogLinkButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const CatalogLinkButton = ({ className, to, children }) => (
-  <Link to={to} className={`btn ${className}`} role="button">
+export const CatalogLinkButton = ({ className, to = "#", children, onClick }) => (
+  <Link to={to} className={`btn ${className}`} role="button" onClick={onClick}>
     {children}
   </Link>
 );
--- src/components/NavCatalog/NavCatalog.jsx
+++ src/components/NavCatalog/NavCatalog.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTag } from '../../core/store/tagsSlice';
 import cn from 'classnames';
 import s from './styles.module.scss';
 import { CatalogLinkButton as LinkButton } from './CatalogLinkButton';
 
+const tagNames = [
+  "Без глютена",
+  "Без сахара",
+  "Вегетарианское",
+  "Без лактозы",
+  "Завтрак",
+  "Гарниры",
+  "Супы",
+  "Еще"
+];
+
 export const NavCatalog = () => {
+  const activeTags = useSelector((state) => state.tags);
+  const dispatch = useDispatch();
+
+  const handleTagClick = (tagIndex) => {
+    dispatch(toggleTag(tagIndex));
+  };
+
   return (
     <div className={s.container}>
       <div className={s.catalog__navigation}>
-        <LinkButton to="/" className={s.catalog__navigationItem}>
-          Без глютена
-        </LinkButton>
-        <LinkButton to="/" className={s.catalog__navigationItem}>
-          Без сахара
-        </LinkButton>
-        <LinkButton to="/" className={s.catalog__navigationItem}>
-          Вегетарианское
-        </LinkButton>
-        <LinkButton to="/" className={s.catalog__navigationItem}>
-          Без лактозы
-        </LinkButton>
-        <LinkButton to="/" className={s.catalog__navigationItem}>
-          Завтрак
-        </LinkButton>
-        <LinkButton to="/" className={s.catalog__navigationItem}>
-          Гарниры
-        </LinkButton>
-        <LinkButton to="/" className={s.catalog__navigationItem}>
-          Супы
-        </LinkButton>
-        <LinkButton to="/" className={s.catalog__navigationItem}>
-          Еще
-        </LinkButton>
+        {tagNames.map((tagName, index) => (
+          <LinkButton
+            key={index}
+            className={cn(s.catalog__navigationItem, {
+              [s.active]: activeTags.includes(index)
+            })}
+            onClick={() => handleTagClick(index)}
+          >
+            {tagName}
+          </LinkButton>
+        ))}
       </div>
     </div>
   );
