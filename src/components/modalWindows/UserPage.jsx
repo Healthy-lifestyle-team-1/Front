469599cd5b79ee21a9f103c376f69cea1efbde82
dnszhkv <den1szhuk0v@yandex.ru>
2024-06-16 23:51:17 +0700

    Update UserPage.jsx

--- src/components/modalWindows/UserPage.jsx
+++ src/components/modalWindows/UserPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import s from './styles.module.scss';
 import Theme from '../../assets/styles/themes/index';
