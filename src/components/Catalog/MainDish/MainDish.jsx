ea00200478bb499ad09a2ce3aa4eb39b765570de
Veronika <xafling@mail.ru>
2024-06-17 01:44:17 +0300

    added api in Catalog

--- src/components/Catalog/MainDish/MainDish.jsx
+++ src/components/Catalog/MainDish/MainDish.jsx
@@ -1,41 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './styles.module.scss';
 import { CardCatalog } from '../../ui/Cards/CardCatalog';
 
-import plateImg from '../../../assets/images/halfofplates/left/card-catalog-img.png';
-
-const plates = [
-  {
-    title: 'Рыба и салат',
-    extra: 'в томатном соусе с молодым картофелем в укропе',
-    weight: '560 г',
-    calories: '675 ккал',
-    img: plateImg,
-  },
-  {
-    title: 'Котлеты с рисом',
-    extra: 'в сливочном соусе',
-    weight: '500 г',
-    calories: '600 ккал',
-    img: plateImg,
-  },
-  {
-    title: 'Рыба и салат',
-    extra: 'в томатном соусе с молодым картофелем в укропе',
-    weight: '560 г',
-    calories: '675 ккал',
-    img: plateImg,
-  },
-  {
-    title: 'Котлеты с рисом',
-    extra: 'в сливочном соусе',
-    weight: '500 г',
-    calories: '600 ккал',
-    img: plateImg,
-  },
-];
-
 export const MainDish = () => {
+  const [plates, setPlates] = useState([]);
+
+  useEffect(() => {
+    const fetchPlates = async () => {
+      try {
+        const response = await fetch(
+          'https://grikoandrey.pythonanywhere.com/product/?name=&category=3&calories=&proteins=&fats=&carbs=&price=&is_prepared=unknown&not_name=&not_calories=&not_proteins=&not_fats=&not_carbs=&not_price=&not_is_prepared=unknown',
+        );
+        const data = await response.json();
+        const formattedData = data.map(item => ({
+          title: item.name,
+          extra: item.cooking_method,
+          weight: `${item.weight} г`,
+          calories: `${item.calories} ккал`,
+          img: item.image,
+        }));
+        setPlates(formattedData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchPlates();
+  }, []);
+
   return (
     <div className={s.container}>
       <p className={s.maindish__title}>Основное блюдо</p>
