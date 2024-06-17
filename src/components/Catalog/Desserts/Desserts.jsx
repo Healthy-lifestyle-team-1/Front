4ad1ef68519bc998099ee20a80bf473f8ebd52cf
Veronika <xafling@mail.ru>
2024-06-17 11:37:03 +0300

    added base_url

--- src/components/Catalog/Desserts/Desserts.jsx
+++ src/components/Catalog/Desserts/Desserts.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import s from './styles.module.scss';
 import { CardCatalog } from '../../ui/Cards/CardCatalog/CardCatalog';
+import { BASE_URL } from '../../../core/url';
 
 export const Desserts = () => {
   const [plates, setPlates] = useState([]);
@@ -9,7 +10,7 @@ export const Desserts = () => {
     const fetchPlates = async () => {
       try {
         const response = await fetch(
-          'https://grikoandrey.pythonanywhere.com/product/?name=&category=6&calories=&proteins=&fats=&carbs=&price=&is_prepared=unknown&not_name=&not_calories=&not_proteins=&not_fats=&not_carbs=&not_price=&not_is_prepared=unknown',
+          `${BASE_URL}product/?name=&category=6&calories=&proteins=&fats=&carbs=&price=&is_prepared=unknown&not_name=&not_calories=&not_proteins=&not_fats=&not_carbs=&not_price=&not_is_prepared=unknown`,
         );
         const data = await response.json();
         const formattedData = data.map(item => ({
--- src/components/Catalog/Popular/Popular.jsx
+++ src/components/Catalog/Popular/Popular.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import s from './styles.module.scss';
 import { CardCatalog } from '../../ui/Cards/CardCatalog';
+import { BASE_URL } from '../../../core/url';
 
 export const Popular = () => {
   const [plates, setPlates] = useState([]);
@@ -9,7 +10,7 @@ export const Popular = () => {
     const fetchPlates = async () => {
       try {
         const response = await fetch(
-          'https://grikoandrey.pythonanywhere.com/product/?name=&category=1&calories=&proteins=&fats=&carbs=&price=&is_prepared=unknown&not_name=&not_calories=&not_proteins=&not_fats=&not_carbs=&not_price=&not_is_prepared=unknown',
+          `${BASE_URL}/product/?name=&category=1&calories=&proteins=&fats=&carbs=&price=&is_prepared=unknown&not_name=&not_calories=&not_proteins=&not_fats=&not_carbs=&not_price=&not_is_prepared=unknown`,
         );
         const data = await response.json();
         const formattedData = data.map(item => ({
