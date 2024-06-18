f0c41b3c7410ee24d79fadd6d4639164135e405e
Name <kattie@outlook.com>
2024-06-18 22:12:52 +0200

    imgs arc slider added

--- src/components/ui/Sliders/ArcSlider/ArcSlider.jsx
+++ src/components/ui/Sliders/ArcSlider/ArcSlider.jsx
@@ -28,7 +28,8 @@ const plates = [
     price: '1900 ₽',
   },
   {
-    title: 'Слабосоленая семга с салатом из помидоров, моцареллы, авокадо и рукколы',
+    title:
+      'Слабосоленая семга с салатом из помидоров, моцареллы, авокадо и рукколы',
     description:
       'Изысканное блюдо с слабосоленой семгой и нежным салатом создает гармоничное сочетание вкусов и текстур',
     weight: '460 г',
@@ -64,7 +65,8 @@ const plates = [
     price: '1400 ₽',
   },
   {
-    title: 'Куриная грудка с грибами c картофельным пюре, кукурузой и винегретом',
+    title:
+      'Куриная грудка с грибами c картофельным пюре, кукурузой и винегретом',
     description:
       'Золотистая куриная грудка с грибами подается на гарнире из кремового картофельного пюре, с добавлением кукурузы и легкого винегрета',
     weight: '510 г',
@@ -93,202 +95,204 @@ const plates = [
 ];
 
 export const ArcSlider = () => {
-	const [activeIndex, setActiveIndex] = useState(0);
-	const theme = useSelector(state => state.theme);
-	let tl = gsap.timeline({ paused: true, reversed: true });
- 
-	const updatePathAndItems = () => {
-	  const circlePath = MotionPathPlugin.convertToPath('#holder', false)[0];
-	  circlePath.id = 'circlePath';
-	  const svg = document.querySelector('svg');
-	  if (svg) {
-		 svg.prepend(circlePath);
-	  }
- 
-	  let items = gsap.utils.toArray(`.${s.item}`);
-	  let numItems = items.length;
-	  let itemStep = 1 / numItems;
-	  let wrapProgress = gsap.utils.wrap(0, 1);
-	  let snap = gsap.utils.snap(itemStep);
-	  let wrapTracker = gsap.utils.wrap(0, numItems);
-	  let tracker = { item: 0 };
- 
-	  gsap.set(items, {
-		 motionPath: {
-			path: circlePath,
-			align: circlePath,
-			alignOrigin: [0.5, 0.5],
-			end: i => i / items.length,
-		 },
-		 scale: 0.6,
-	  });
- 
-	  gsap.set(`.${s.arc__block}`, {
-		 rotation: 0,
-		 transformOrigin: 'center',
-	  });
- 
-	  gsap.set(items, {
-		 rotation: 180,
-		 transformOrigin: 'center',
-	  });
- 
-	  tl.clear();
-	  tl = gsap.timeline({ paused: true, reversed: true });
- 
-	  tl.to(`.${s.arc__block}`, {
-		 rotation: 360,
-		 transformOrigin: 'center',
-		 duration: 1,
-		 ease: 'none',
-	  });
- 
-	  tl.to(
-		 items,
-		 {
-			rotation: '-=360',
-			transformOrigin: 'center',
-			duration: 1,
-			ease: 'none',
-		 },
-		 0,
-	  );
- 
-	  tl.to(
-		 tracker,
-		 {
-			item: numItems,
-			duration: 1,
-			ease: 'none',
-			modifiers: {
-			  item(value) {
-				 return wrapTracker(numItems - Math.round(value));
-			  },
-			},
-		 },
-		 0,
-	  );
- 
-	  const moveWheel = amount => {
-		 const progress = tl.progress();
-		 tl.progress(wrapProgress(snap(tl.progress() + amount)));
-		 const next = tracker.item;
-		 tl.progress(progress);
- 
-		 gsap.to(tl, {
-			progress: snap(tl.progress() + amount),
-			modifiers: {
-			  progress: wrapProgress,
-			},
-		 });
- 
-		 setActiveIndex(next);
-	  };
- 
-	  items.forEach((el, i) => {
-		 el.addEventListener('click', () => {
-			const current = tracker.item;
-			const diff = current - i;
- 
-			if (Math.abs(diff) < numItems / 2) {
-			  moveWheel(diff * itemStep);
-			} else {
-			  const amt = numItems - Math.abs(diff);
- 
-			  if (current > i) {
-				 moveWheel(amt * -itemStep);
-			  } else {
-				 moveWheel(amt * itemStep);
-			  }
-			}
-		 });
-	  });
- 
-	  document
-		 .getElementById('next')
-		 ?.addEventListener('click', () => moveWheel(-itemStep));
-	  document
-		 .getElementById('prev')
-		 ?.addEventListener('click', () => moveWheel(itemStep));
- 
-	  return moveWheel;
-	};
- 
-	useEffect(() => {
-	  let moveWheel = updatePathAndItems();
- 
-	  const handleResize = () => {
-		 tl.progress(0);
-		 tl.clear();
-		 moveWheel = updatePathAndItems();
-	  };
- 
-	  window.addEventListener('resize', handleResize);
- 
-	  const intervalId = setInterval(() => {
-		 moveWheel(-1 / plates.length);
-	  }, 12000);
- 
-	  return () => {
-		 window.removeEventListener('resize', handleResize);
-		 clearInterval(intervalId);
-	  };
-	}, []);
- 
-	return (
-	  <div className={s.arcSlider__container}>
-		 <div className={s.arc__wrapper}>
-			<div className={s.arc__block}>
-			  {plates.map((plate, index) => (
-				 <div
-					key={index}
-					className={cn(s.item, { [s.active]: activeIndex === index })}
-				 >
-					<img className={s.itemImg} src={plate.img} alt={plate.title} />
-				 </div>
-			  ))}
-			  <svg className={s.svg} viewBox="0 0 400 400">
-				 <circle
-					id="holder"
-					className={s.st0}
-					cx="200"
-					cy="200"
-					r="139"
-					fill="none"
-					stroke="red" strokeWidth="1" strokeDasharray="5,5"
-				 />
-			  </svg>
-			</div>
-		 </div>
-		 <div className={s.arcSlider__infoBlock}>
-			<div className={s.arcSlider__infoBlock__title}>
-			  Сбалансированные{' '}
-			  <span className={s.arcSlider__infoBlock__title__pink}>готовые</span>{' '}
-			  блюда
-			</div>
-			<div className={s.arcSlider__infoBlock__dishName}>
-			  {plates[activeIndex].title}
-			</div>
-			<div className={s.arcSlider__infoBlock__description}>
-			  {plates[activeIndex].description}
-			</div>
-			<div className={s.arcSlider__infoBlock__nutritionalValues}>
-			  <div className={s.arcSlider__infoBlock__nutritionalValues__weight}>
-				 {plates[activeIndex].weight}
-			  </div>
-			  <div className={s.arcSlider__infoBlock__nutritionalValues__calories}>
-				 {plates[activeIndex].calories}
-			  </div>
-			</div>
-			<Button
-			  title={plates[activeIndex].price}
-			  onClick={() => console.log('Button clicked')}
-			  colorScheme={1}
-			  size={1}
-			/>
-		 </div>
-	  </div>
-	);
- };
- 
- export default ArcSlider;
+  const [activeIndex, setActiveIndex] = useState(0);
+  const theme = useSelector(state => state.theme);
+  let tl = gsap.timeline({ paused: true, reversed: true });
+
+  const updatePathAndItems = () => {
+    const circlePath = MotionPathPlugin.convertToPath('#holder', false)[0];
+    circlePath.id = 'circlePath';
+    const svg = document.querySelector('svg');
+    if (svg) {
+      svg.prepend(circlePath);
+    }
+
+    let items = gsap.utils.toArray(`.${s.item}`);
+    let numItems = items.length;
+    let itemStep = 1 / numItems;
+    let wrapProgress = gsap.utils.wrap(0, 1);
+    let snap = gsap.utils.snap(itemStep);
+    let wrapTracker = gsap.utils.wrap(0, numItems);
+    let tracker = { item: 0 };
+
+    gsap.set(items, {
+      motionPath: {
+        path: circlePath,
+        align: circlePath,
+        alignOrigin: [0.5, 0.5],
+        end: i => i / items.length,
+      },
+      scale: 0.6,
+    });
+
+    gsap.set(`.${s.arc__block}`, {
+      rotation: 0,
+      transformOrigin: 'center',
+    });
+
+    gsap.set(items, {
+      rotation: 180,
+      transformOrigin: 'center',
+    });
+
+    tl.clear();
+    tl = gsap.timeline({ paused: true, reversed: true });
+
+    tl.to(`.${s.arc__block}`, {
+      rotation: 360,
+      transformOrigin: 'center',
+      duration: 1,
+      ease: 'none',
+    });
+
+    tl.to(
+      items,
+      {
+        rotation: '-=360',
+        transformOrigin: 'center',
+        duration: 1,
+        ease: 'none',
+      },
+      0,
+    );
+
+    tl.to(
+      tracker,
+      {
+        item: numItems,
+        duration: 1,
+        ease: 'none',
+        modifiers: {
+          item(value) {
+            return wrapTracker(numItems - Math.round(value));
+          },
+        },
+      },
+      0,
+    );
+
+    const moveWheel = amount => {
+      const progress = tl.progress();
+      tl.progress(wrapProgress(snap(tl.progress() + amount)));
+      const next = tracker.item;
+      tl.progress(progress);
+
+      gsap.to(tl, {
+        progress: snap(tl.progress() + amount),
+        modifiers: {
+          progress: wrapProgress,
+        },
+      });
+
+      setActiveIndex(next);
+    };
+
+    items.forEach((el, i) => {
+      el.addEventListener('click', () => {
+        const current = tracker.item;
+        const diff = current - i;
+
+        if (Math.abs(diff) < numItems / 2) {
+          moveWheel(diff * itemStep);
+        } else {
+          const amt = numItems - Math.abs(diff);
+
+          if (current > i) {
+            moveWheel(amt * -itemStep);
+          } else {
+            moveWheel(amt * itemStep);
+          }
+        }
+      });
+    });
+
+    document
+      .getElementById('next')
+      ?.addEventListener('click', () => moveWheel(-itemStep));
+    document
+      .getElementById('prev')
+      ?.addEventListener('click', () => moveWheel(itemStep));
+
+    return moveWheel;
+  };
+
+  useEffect(() => {
+    let moveWheel = updatePathAndItems();
+
+    const handleResize = () => {
+      tl.progress(0);
+      tl.clear();
+      moveWheel = updatePathAndItems();
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    const intervalId = setInterval(() => {
+      moveWheel(-1 / plates.length);
+    }, 12000);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div className={s.arcSlider__container}>
+      <div className={s.arc__wrapper}>
+        <div className={s.arc__block}>
+          {plates.map((plate, index) => (
+            <div
+              key={index}
+              className={cn(s.item, { [s.active]: activeIndex === index })}
+            >
+              <img className={s.itemImg} src={plate.img} alt={plate.title} />
+            </div>
+          ))}
+          <svg className={s.svg} viewBox="0 0 400 400">
+            <circle
+              id="holder"
+              className={s.st0}
+              cx="200"
+              cy="200"
+              r="139"
+              fill="none"
+              stroke="red"
+              strokeWidth="1"
+              strokeDasharray="5,5"
+            />
+          </svg>
+        </div>
+      </div>
+      <div className={s.arcSlider__infoBlock}>
+        <div className={s.arcSlider__infoBlock__title}>
+          Сбалансированные{' '}
+          <span className={s.arcSlider__infoBlock__title__pink}>готовые</span>{' '}
+          блюда
+        </div>
+        <div className={s.arcSlider__infoBlock__dishName}>
+          {plates[activeIndex].title}
+        </div>
+        <div className={s.arcSlider__infoBlock__description}>
+          {plates[activeIndex].description}
+        </div>
+        <div className={s.arcSlider__infoBlock__nutritionalValues}>
+          <div className={s.arcSlider__infoBlock__nutritionalValues__weight}>
+            {plates[activeIndex].weight}
+          </div>
+          <div className={s.arcSlider__infoBlock__nutritionalValues__calories}>
+            {plates[activeIndex].calories}
+          </div>
+        </div>
+        <Button
+          title={plates[activeIndex].price}
+          onClick={() => console.log('Button clicked')}
+          colorScheme={1}
+          size={1}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ArcSlider;
