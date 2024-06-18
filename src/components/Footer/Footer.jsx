dbf04a6e9cc90e56cffbff9daf40ce64a213f595
Name <kattie@outlook.com>
2024-06-18 15:43:13 +0200

    animation tuned

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,15 +10,16 @@ import QR from '../../assets/images/qr.png';
 
 export const Footer = () => {
   const isAuthorize = useSelector(state => state.auth.isAuthorize);
+  const theme = useSelector(state => state.theme); // Предположим, что тема хранится в state.theme
 
   return (
-    <div className={s.footer__container}>
+    <div className={cn(s.footer__container, { [s.dark]: theme === 'dark', [s.light]: theme !== 'dark' })}>
       <div className={s.footer__wrapper}>
-          <div className={s.footer__logo}>
-            <Logo />
-          </div>
-			 <div className={s.footer__navBlock}>
-			 <div className={s.footer__navList__one}>
+        <div className={s.footer__logo}>
+          <Logo />
+        </div>
+        <div className={s.footer__navBlock}>
+          <div className={s.footer__navList__one}>
             <a className={s.footer__navList__item} href="">
               Главная
             </a>
@@ -31,10 +32,9 @@ export const Footer = () => {
             <a className={s.footer__navList__item} href="">
               Книга
             </a>
-				
-				</div>
-				<div className={s.footer__navList__two}>
-				<a className={s.footer__navList__item} href="">
+          </div>
+          <div className={s.footer__navList__two}>
+            <a className={s.footer__navList__item} href="">
               Статьи
             </a>
             {isAuthorize ? (
@@ -51,58 +51,50 @@ export const Footer = () => {
                 Войти
               </a>
             )}
-				</div>
-				<div className={s.footer__navList__three}>
-				<span className={s.footer__navList__item} href="">
+          </div>
+          <div className={s.footer__navList__three}>
+            <span className={s.footer__navList__item} href="">
               Связаться с нами :
             </span>
-				<a className={s.footer__navList__item} href="">
-				+7 999 999 99 99
+            <a className={s.footer__navList__item} href="">
+              +7 999 999 99 99
             </a>
-				<a className={s.footer__navList__item} href="">
-				zozhnik@gmail.com
+            <a className={s.footer__navList__item} href="">
+              zozhnik@gmail.com
             </a>
-					 </div>
+          </div>
+          <div className={s.footer__navList__four}>
+            <div className={s.footer__socialTitle}>Мы в сетях</div>
+            <div className={s.footer__socialIcons}>
+              <a
+                href="https://www.instagram.com/zozhnik_ru?igsh=MWRoeXFhaDl0eGFlNA=="
+                target="_blank"
+              >
+                <img src={Instagram} alt="иконка Instagram" />
+              </a>
 
-         
+              <a href="https://www.youtube.com/@zozhnik_ru" target="_blank">
+                <img src={YouTube} alt="иконка Youtube" />
+              </a>
 
-            <div className={s.footer__navList__four}>
-              <div className={s.footer__socialTitle}>Мы в сетях</div>
-              <div className={s.footer__socialIcons}>
-                <a
-                  href="https://www.instagram.com/zozhnik_ru?igsh=MWRoeXFhaDl0eGFlNA=="
-                  target="_blank"
-                >
-                  <img src={Instagram} alt="иконка Instagram" />
-                </a>
-
-                <a href="https://www.youtube.com/@zozhnik_ru" target="_blank">
-                  <img src={YouTube} alt="иконка Youtube" />
-                </a>
-
-                <a href="https://t.me/zozhnik" target="_blank">
-                  <img src={Telegram} alt="иконка Telegram" />
-                </a>
-              </div>
+              <a href="https://t.me/zozhnik" target="_blank">
+                <img src={Telegram} alt="иконка Telegram" />
+              </a>
             </div>
-
-          
-
+          </div>
           <div className={s.footer__navList__five}>
-				<span className={s.footer__navList__item} >Скачай с помощью</span>
+            <span className={s.footer__navList__item}>Скачай с помощью</span>
             <img className={s.footer__qrImg} src={QR} alt="qr" />
           </div>
-			 </div>
-			 
-
-      <div className={s.footer__rightsText}>
-        Осуществляя вход на этот Сайт/в мобильное приложение ЗОЖНИК, вы
-        подтверждаете, что ознакомлены с Пользовательским
-        соглашением и Положением по обработке и защите персональных данных. С
-        общими правилами участия в акциях и порядке получения подарков Вы
-        можете ознакомиться здесь
+        </div>
+        <div className={s.footer__rightsText}>
+          Осуществляя вход на этот Сайт/в мобильное приложение ЗОЖНИК, вы
+          подтверждаете, что ознакомлены с Пользовательским
+          соглашением и Положением по обработке и защите персональных данных. С
+          общими правилами участия в акциях и порядке получения подарков Вы
+          можете ознакомиться здесь
+        </div>
       </div>
-		</div>
-	 </div>
+    </div>
   );
 };
