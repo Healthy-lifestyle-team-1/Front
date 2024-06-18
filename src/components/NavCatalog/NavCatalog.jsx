4068147a52facb6c4b54fb790bf1d539be165200
Veronika <xafling@mail.ru>
2024-06-18 13:24:21 +0300

    (добавила объединение тегов)

--- src/components/NavCatalog/NavCatalog.jsx
+++ src/components/NavCatalog/NavCatalog.jsx
@@ -1,5 +1,5 @@
 // src/components/NavCatalog/index.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTag } from '../../core/store/tagsSlice';
 import cn from 'classnames';
@@ -29,26 +29,36 @@ export const NavCatalog = () => {
   const activeTags = useSelector(state => state.tags);
   const dispatch = useDispatch();
 
-  const handleTagClick = async tagIndex => {
+  const handleTagClick = tagIndex => {
     dispatch(toggleTag(tagIndex));
-
-    if (tagIndex in tagToFilterParam) {
-      const filterParam = tagToFilterParam[tagIndex];
-      try {
-        const response = await fetch(
-          `${BASE_URL}/product/?name=&calories=&proteins=&fats=&carbs=&price=&is_prepared=&not_name=&not_calories=&not_proteins=&not_fats=&not_carbs=&not_price=&not_is_prepared=&not_tag=${filterParam}`,
-        );
-        if (response.ok) {
-          const data = await response.json();
-          console.log(`Количество найденных объектов: ${data.length}`);
-        } else {
-          console.error('Ошибка при получении данных');
-        }
-      } catch (error) {
-        console.error('Ошибка при выполнении запроса:', error);
-      }
-    }
   };
+
+  useEffect(() => {
+    const fetchFilteredProducts = async () => {
+      const activeFilterParams = activeTags
+        .filter(tagIndex => tagIndex in tagToFilterParam)
+        .map(tagIndex => tagToFilterParam[tagIndex])
+        .join('&not_tag=');
+
+      if (activeFilterParams) {
+        try {
+          const response = await fetch(
+            `${BASE_URL}/product/?not_tag=${activeFilterParams}`,
+          );
+          if (response.ok) {
+            const data = await response.json();
+            console.log(`Количество найденных объектов: ${data.length}`);
+          } else {
+            console.error('Ошибка при получении данных');
+          }
+        } catch (error) {
+          console.error('Ошибка при выполнении запроса:', error);
+        }
+      }
+    };
+
+    fetchFilteredProducts();
+  }, [activeTags]);
 
   return (
     <div className={s.container}>
