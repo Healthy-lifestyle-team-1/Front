c5c352989908f48a068a26f1b57c78c13d143c41
victoria <itsfoxiefo@gmail.com>
2024-06-19 20:49:02 +0200

    tuned OnBoarding

--- src/components/Auth/OnBoarding/OnBoarding.jsx
+++ src/components/Auth/OnBoarding/OnBoarding.jsx
@@ -3,6 +3,10 @@ import { Button } from '../../ui/Button';
 import x from '../../../assets/images/icons/light/X.svg';
 import s from './styles.module.scss';
 
+import plate1 from '../../../assets/images/OnBoarding/plate1.png'
+import plate2 from '../../../assets/images/OnBoarding/plate2.png'
+import book from '../../../assets/images/OnBoarding/book.png'
+
 export const OnBoarding = ({ onClose }) => {
   const [step, setStep] = useState(1); // 1 - balanced method, 2 - build your plate, 3 - nutrition ideology, 4 - exclude from plates
 
@@ -20,14 +24,14 @@ export const OnBoarding = ({ onClose }) => {
     switch (step) {
       case 1:
         return (
-          <div className={s.onBoarding__content}>
+          <div className={s.onBoarding__contentOne}>
             <div className={s.onBoarding__title}>
-              <span>МЕТОД СБАЛАНСИРОВАННОЙ </span>
-              <span>ТАРЕЛКИ</span>
+              <span>МЕТОД<br/>СБАЛАНСИРОВАННОЙ<br/></span>
+              <span className={s.onBoarding__titleWhite}>ТАРЕЛКИ</span>
             </div>
-            {/* <div className={s.onBoarding__img}>
-              <img src={Frame1} alt={'Тарелка'} />
-            </div> */}
+            <div className={s.onBoarding__img}>
+              <img src={plate1} alt={'Тарелка'} />
+            </div>
             <div className={s.onBoarding__text}>
               Тарелки собираются с расчётом необходимых калорий, белков, жиров,
               углеводов, а также микроэлементов
@@ -42,14 +46,14 @@ export const OnBoarding = ({ onClose }) => {
         );
       case 2:
         return (
-          <div className={s.onBoarding__content}>
+          <div className={s.onBoarding__contentTwo}>
             <div className={s.onBoarding__title}>
               <span>СОБЕРИ СВОЮ </span>
               <span>ЗДОРОВУЮ ТАРЕЛКУ</span>
             </div>
-            {/* <div className={s.onBoarding__img}>
-              <img src={Frame2} alt={'Конструктор'} />
-            </div> */}
+            <div className={s.onBoarding__img}>
+              <img src={plate2} alt={'Конструктор'} />
+            </div>
             <div className={s.onBoarding__text}>
               Собери тарелку из двух половинок с любимыми ингредиентами на свой
               вкус, и можно исключить свои аллергены с помощью тегов
@@ -64,14 +68,14 @@ export const OnBoarding = ({ onClose }) => {
         );
       case 3:
         return (
-          <div className={s.onBoarding__content}>
+          <div className={s.onBoarding__contentThree}>
             <div className={s.onBoarding__title}>
-              <span>ИДЕОЛОГИЯ</span>
-              <span>ЗДОРОВОГО ПИТАНИЯ</span>
+              <span>ИДЕОЛОГИЯ<br/></span>
+              <span><span className={s.onBoarding__titleWhite}>ЗДОРОВОГО</span> ПИТАНИЯ</span>
             </div>
-            {/* <div className={s.onBoarding__img}>
-              <img src={Frame3} alt={'Книга рецептов'} />
-            </div> */}
+            <div className={s.onBoarding__img}>
+              <img src={book} alt={'Книга рецептов'} />
+            </div>
             <div className={s.onBoarding__text}>
               Современные рекомендации по правильному сбалансированному питанию
               на научной основе
@@ -86,7 +90,7 @@ export const OnBoarding = ({ onClose }) => {
         );
       case 4:
         return (
-          <div className={s.onBoarding__content}>
+          <div className={s.onBoarding__contentFour}>
             <div className={s.onBoarding__title}>
               <span>ЧТО НАМ ИСКЛЮЧИТЬ</span>
               <span>ИЗ ТАРЕЛОК?</span>
@@ -113,9 +117,9 @@ export const OnBoarding = ({ onClose }) => {
 
   return (
     <div className={s.modalOverlay} onClick={handleOverlayClick}>
-      <div className={s.modal__content}>
+      <div className={`${s.modal__content} ${s[`modal__content${step}`]}`}>
         <button className={s.closeButton} onClick={onClose}>
-          <img src={x} alt={'Закрыть'} />
+          закрыть
         </button>
         {getContent()}
       </div>
